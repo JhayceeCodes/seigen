abcc090fe0ff3409d3448736469e63a6378ad0db
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-07-20 23:15:42 +0100

    feat: isolate health function from main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/JhayceeCodes/rate-limiter-gateway/internal/handler"
 )
-
-func health(w http.ResponseWriter, r *http.Request) {
-
-	response := map[string]string{
-		"message": "Server running fine",
-		"status":  "ok",
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(response)
-
-}
 
 func main() {
 
-	http.HandleFunc("/health", health)
+	http.HandleFunc("/health", handler.Health)
 
 	fmt.Println("Server running on 8080.")
 
