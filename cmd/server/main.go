41a6667f2f01c37b055a2e9a6e0b01a69703c01f
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-07-20 22:02:27 +0100

    feat: rewrite main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,20 +6,25 @@ import (
 	"net/http"
 )
 
-func Health(w http.ResponseWriter, r *http.Request) {
+func health(w http.ResponseWriter, r *http.Request) {
+
 	response := map[string]string{
-		"status": "ok",
+		"message": "Server running fine",
+		"status":  "ok",
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode((response))
+	json.NewEncoder(w).Encode(response)
+
 }
 
 func main() {
-	http.HandleFunc("/health", Health)
 
-	fmt.Println("Server running on: 8080")
+	http.HandleFunc("/health", health)
+
+	fmt.Println("Server running on 8080.")
 
 	http.ListenAndServe(":8080", nil)
+
 }
