a424c9452a4b9b64ed8d92b0e7bc8b57dcf83c86
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-08-01 00:30:38 +0100

    feat(limiter): implement sliding window algorithm

--- internal/limiter/sliding_window.go
+++ internal/limiter/sliding_window.go
@@ -1,1 +1,81 @@
 package limiter
+
+import (
+	"sync"
+	"time"
+)
+
+type SlidingWindow struct {
+	requests []time.Time
+	limit    int
+	window   time.Duration
+	mu       sync.Mutex
+}
+
+func NewSlidingWindow(limit int, window time.Duration) *SlidingWindow {
+	if limit <= 0 {
+		panic("capacity cannot be less than zero")
+	}
+
+	if window <= 0 {
+		panic("window must be greater than zero")
+	}
+
+	return &SlidingWindow{
+		limit:  limit,
+		window: window,
+	}
+}
+
+// cleanUp removes expired timestamps.
+//
+// Caller must hold sw.mu.
+func (sw *SlidingWindow) cleanUp() {
+	currentTime := time.Now()
+
+	cutoff := currentTime.Add(-sw.window)
+
+	for len(sw.requests) > 0 {
+		if sw.requests[0].Before(cutoff) {
+			sw.requests = sw.requests[1:]
+		} else {
+			break
+		}
+	}
+}
+
+func (sw *SlidingWindow) Allow() bool {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	sw.cleanUp()
+
+	if len(sw.requests) >= sw.limit {
+		return false
+	}
+
+	sw.requests = append(sw.requests, time.Now())
+	return true
+}
+
+// Requests returns the number of active requests
+// after removing expired timestamps.
+func (sw *SlidingWindow) Requests() int {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	sw.cleanUp()
+
+	return len(sw.requests)
+}
+
+// Remaining returns the remaining request capacity
+// in the current sliding window.
+func (sw *SlidingWindow) Remaining() int {
+	sw.mu.Lock()
+	defer sw.mu.Unlock()
+
+	sw.cleanUp()
+
+	return sw.limit - len(sw.requests)
+}
