f330e472cc64f0c027e87c612198a96417908c57
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-08-17 16:20:13 +0100

    feat(model): implement validation check for limiter algorithms and configurations

--- internal/model/limiter_config.go
+++ internal/model/limiter_config.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type Algorithm string
 
@@ -42,3 +45,58 @@ type LeakyBucketConfig struct {
 }
 
 func (LeakyBucketConfig) isLimiterConfig() {}
+
+func (c *LimiterConfig) Validate() error {
+	switch c.Algorithm {
+	case TokenBucket:
+		config, ok := c.Config.(TokenBucketConfig)
+		if !ok {
+			return errors.New("token bucket requires TokenBucketConfig")
+		}
+
+		if config.Capacity <= 0 {
+			return errors.New("token bucket capacity must be greater than zero")
+		}
+
+		if config.RefillInterval <= 0 {
+			return errors.New("token bucket refill interval must be greater than zero")
+		}
+
+		if config.RefillAmount <= 0 {
+			return errors.New("token bucket refill amount must be greater than zero")
+		}
+
+	case LeakyBucket:
+		config, ok := c.Config.(LeakyBucketConfig)
+		if !ok {
+			return errors.New("leaky bucket requires LeakyBucketConfig")
+		}
+
+		if config.Capacity <= 0 {
+			return errors.New("leaky bucket capacity must be greater than zero")
+		}
+
+		if config.LeakInterval <= 0 {
+			return errors.New("leaky bucket leak interval must be greater than zero")
+		}
+
+	case FixedWindow, SlidingWindowLog, SlidingWindowCounter:
+		config, ok := c.Config.(WindowConfig)
+		if !ok {
+			return errors.New("window-based limiter requires WindowConfig")
+		}
+
+		if config.Limit <= 0 {
+			return errors.New("window limit must be greater than zero")
+		}
+
+		if config.Window <= 0 {
+			return errors.New("window duration must be greater than zero")
+		}
+
+	default:
+		return errors.New("unknown limiter algorithm")
+	}
+
+	return nil
+}
