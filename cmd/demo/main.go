612d24c572a189d818e658b0b0e949faa0c0dc4d
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-08-19 17:31:34 +0100

    chore: test policy store functionality in main

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/JhayceeCodes/seigen/internal/limiter"
 	"github.com/JhayceeCodes/seigen/internal/middleware"
+	"github.com/JhayceeCodes/seigen/internal/model"
+	"github.com/JhayceeCodes/seigen/internal/store"
 )
 
 func Hello(w http.ResponseWriter, r *http.Request) {
@@ -16,16 +18,39 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-
 	json.NewEncoder(w).Encode(response)
-
 }
 
 func main() {
+	policyStore := store.NewPolicyStore()
+
+	policy := model.Policy{
+		Identifier: "user:123",
+		Limiter: model.LimiterConfig{
+			Algorithm: model.TokenBucket,
+			Config: model.TokenBucketConfig{
+				Capacity:       3,
+				RefillInterval: 10 * time.Second,
+				RefillAmount:   1,
+			},
+		},
+	}
+
+	if err := policyStore.Set(policy); err != nil {
+		panic(err)
+	}
+
+	policy, err := policyStore.Get("user:123")
+	if err != nil {
+		panic(err)
+	}
+
+	config := policy.Limiter.Config.(model.TokenBucketConfig)
+
 	bucket := limiter.NewTokenBucket(
-		3,
-		30*time.Second,
-		1,
+		config.Capacity,
+		config.RefillInterval,
+		config.RefillAmount,
 	)
 
 	handler := middleware.RateLimit(
