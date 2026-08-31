e0b72c9cda42bfc6d49323f623cdb2d83a430d44
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-09-01 00:38:04 +0100

    fix(migration): replace {} with () and make id sequential

--- migrations/001_create_policies.up.sql
+++ migrations/001_create_policies.up.sql
@@ -1,7 +1,7 @@
-CREATE TABLE policies {
-    id UUID PRIMARY KEY,
+CREATE TABLE policies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     identifier TEXT NOT NULL UNIQUE,
     limiter_config JSONB NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-};
+);
