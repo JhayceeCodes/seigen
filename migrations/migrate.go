10efcdd8cfae2ccc5348043a217aea4e08893d69
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-09-01 22:27:22 +0100

    feat(migrations): implement migration function with golang-migrate

---
+++ migrations/migrate.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"database/sql"
+	"embed"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+)
+
+//go:embed *.sql
+var migrationFiles embed.FS
+
+func Migrate(db *sql.DB) error {
+	source, err := iofs.New(migrationFiles, ".")
+	if err != nil {
+		return err
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithInstance(
+		"iofs",
+		source,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
