4215c53e7ccfa483b775774cb13f3ac550b539a9
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-09-07 13:14:52 +0100

    feat(migrations): add policy_groups and policy_group_members tables

--- migrations/001_create_seigen_policies.up.sql
+++ migrations/001_create_seigen_policies.up.sql
@@ -5,3 +5,21 @@ CREATE TABLE seigen_policies (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+
+CREATE TABLE seigen_policy_groups (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    limiter_config JSONB NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE seigen_policy_group_members (
+    group_id BIGINT NOT NULL REFERENCES seigen_policy_groups(id) ON DELETE CASCADE,
+    identifier TEXT NOT NULL UNIQUE
+);
+
+CREATE INDEX idx_seigen_policy_group_members_group_id
+ON seigen_policy_group_members(group_id);
